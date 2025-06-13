4b6732baf30c949d3f06d727c88b5142479645c0
s30500 <s30500@pjwstk.edu.pl>
2025-06-13 13:09:49 +0200

    Proper validation for body payloads

--- LabAPBD-Test2/DTOs/NewBatchDto.cs
+++ LabAPBD-Test2/DTOs/NewBatchDto.cs
@@ -1,15 +1,27 @@
-﻿namespace LabAPBD_Test2.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabAPBD_Test2.DTOs;
 
 public class NewBatchDto
 {
-    public int Quantity { get; set; }
+    [Required]
+    public int? Quantity { get; set; }
+    
+    [Required]
     public string Species { get; set; }
+    
+    [Required]
     public string Nursery { get; set; }
+    
+    [Required]
     public List<NewBatchResponsibleDto> Responsible { get; set; }
 }
 
 public class NewBatchResponsibleDto
 {
-    public int EmployeeId { get; set; }
+    [Required]
+    public int? EmployeeId { get; set; }
+    
+    [Required]
     public string Role { get; set; }
 }
